2eab8e44fe14e26641b0aa2aa68caeb3cbc33ff7
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-05-23 16:51:06 +0800

    wrote my full unique ptr

--- my_unique_ptr.cpp
+++ my_unique_ptr.cpp
@@ -20,7 +20,6 @@ void do_something(Resource& r) {
 }
 
 template <typename T>
-
 class my_unique_ptr {
     T* t_;
 
@@ -28,11 +27,26 @@ public:
     my_unique_ptr(std::string n) {
         t_ = new T{n};
     }
-    
     ~my_unique_ptr() {
         delete t_;
     }
-
+    
+    my_unique_ptr(const my_unique_ptr& rhs) = delete; // tell the compiler not to copy  
+    
+    my_unique_ptr(my_unique_ptr&& other) {
+        t_ = other.t_;
+        other.t_ = nullptr;
+    }
+    
+    my_unique_ptr& operator=(my_unique_ptr&& other) {
+        t_ = other.t_;
+        other.t_ = nullptr;
+        return *this;
+    }
+    
+    T* get() {
+        return t_;
+    } 
     T* operator->() {
         return t_;
     }
@@ -41,13 +55,27 @@ public:
 
 int main() {
     // Resource* r = new Resource{"ananya"};
-    std::unique_ptr<Resource> p1 = std::make_unique<Resource>("A");
-    p1->use();
+    std::unique_ptr<Resource> p1 = std::make_unique<Resource>("A"); // only use for objects made on heap
+    std::unique_ptr<Resource> p2 = std::move(p1);
+    // p1->use();
+    std::unique_ptr<Resource> p3 = std::make_unique<Resource>("p3 made");
+    p1 = std::move(p3);
+ 
+    p2->use();
+
+    Resource* rc1 = new Resource{"testing resource"};
+    Resource rc2{"stack resource"};
+    
+    // std::unique_ptr<Resource> p3(&rc2); 
+    // p3->use();
  
     // delete r;
 
     my_unique_ptr<Resource> r1{"papa"};
-    r1->use();
+    my_unique_ptr<Resource> r2 = std::move(r1);
+    my_unique_ptr<Resource> r3{"hello r3"};
+    r1 = std::move(r3);
+    r2->use();
     return 0;
 } 
 
