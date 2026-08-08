ee822b1808f45acf164cda7a3778eb22fda65abc
Ananya Kharbanda <ananyakharbanda28@gmail.com>
2026-08-08 15:57:20 +0800

    prac unique ptr

--- revision1/prac_unique_ptr.cpp
+++ revision1/prac_unique_ptr.cpp
@@ -7,4 +7,34 @@ private:
     T* ptr;
 
 public:
+    explicit UniquePtr(T* p) {
+        ptr = P;
+    }
+    
+    ~UniquePtr() {
+        delete ptr;
+    }
+    
+    UniquePtr(const UniquePtr&) = delete;
+    
+    UniquePtr& operator=(const UniquePtr&) = delete;
+        
+    UniquePtr(UniquePtr&& other) noexcept {
+        ptr = other.ptr;
+        other.ptr = nullptr;
+    }
+    
+    UniquePtr& operator=(UniquePtr&& other) noexcept {
+        if (this != &other) {
+            delete ptr;
+        }
+        
+        ptr = other.ptr;
+        other.ptr = nullptr;
+        
+        return *this;
+    }
+
+    T& operator *() const
+     
     
